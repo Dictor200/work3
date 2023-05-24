93ebcb8cc208a0ba3ba0393f870c2d89a608757a
VladysKor <vkorabov228@gmail.com>
2023-05-24 10:59:19 +0300

    Добавьте файлы проекта.

---
+++ Class1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace worl_3
+{
+    class Employee
+    {
+        private static int idCounter = 0;
+        public int id { get; private set; }
+        public string Fullname { get; set; }
+        public int Department { get; set; }
+        public double Salary { get; set; }
+        public Employee()
+        {
+            id = ++idCounter;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace worl_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        Employee employee1 = new Employee();
+            employee1.Fullname = "Косых Иван Андреевич";
+            employee1.Department = 1;
+            employee1.Salary = 500000;
+            Employee employee2 = new Employee();
+        employee2.Fullname = "Комочкоч Денис Александрович";
+            employee2.Department = 2;
+            employee2.Salary = 20000;
+            Console.WriteLine("Сотрудник 1 (id {0}) : {1}, отдел {2}, зарплата {3}", employee1.id, employee1.Fullname, employee1.Department, employee1.Salary);
+            Console.WriteLine("Сотрудник 2 (id {0}): {1}, отдел {2}, зарплата{3}", employee2.id, employee2.Fullname, employee2.Department, employee2.Salary);
+           Console.ReadKey();
+    }
+
+}
+    }
